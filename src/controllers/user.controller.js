9211769710c7d8e676c83c910ce36e22d25fe43b
Carlos Castillo <c.castillo15@ufromail.cl>
2022-07-14 23:19:15 -0400

    probando si el error era por el nombre del archivo

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,17 +1,11 @@
-const db = require("../models");
-const config = require("../config/auth.config");
-const User = db.user;
-const Role = db.role;
-const Op = db.Sequelize.Op;
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+const user = require('../models/user')
 
 /*GET USER BY ID*/
 exports.getUserById= (req, res)=>{
   const {id} = req.params;
-  User.findById(id).then((data)=> res.json(data)).catch((error)=> res.json({message:error}));
+  user.findById(id).then((data)=> res.json(data)).catch((error)=> res.json({message:error}));
 };
 /*GET ALL USERS*/
 exports.getUsers = (req, res)=>{
-    User.find().then((data) => res.json(data)).catch((error) => res.json({ message: error }));
+  user.find().then((data) => res.json(data)).catch((error) => res.json({ message: error }));
 };
